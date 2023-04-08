94142840abbd3441a7f84e3ea32553ae5fc5c4b7
nora <nborcsok@dev-id.fr>
2023-04-08 22:39:40 +0200

    phase 3: styling with parameters

--- app/src/main/java/com/example/postcard/MainActivity.kt
+++ app/src/main/java/com/example/postcard/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.postcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.postcard.ui.theme.PostcardTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +29,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xFFFFFFFF)
                 ) {
-                    // TODO: 1. open the Split view to see the preview of "MyComposable" within Android Studio
                     MyComposable()
                 }
             }
@@ -31,30 +38,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyComposable() {
+    Box(Modifier.fillMaxSize()) {
+        // TODO: 1. add a Modifier to the image to fill the max height,
+        //            modifier = Modifier.fillMaxHeight(),
 
-    Column {
-        Text(text = "GREETINGS FROM")
-        Text(text = "MARSEILLE")
-    }
-
-    Row {
-        // TODO: 2. copy the texts in here to show them in a row, and comment out the column
-    }
-
-    // TODO: 3. uncomment the below code to show the texts in a box,
-    //  then change the order of the two texts
-/*    Box {
-        Text(
-            text = "GREETINGS FROM",
-            modifier = Modifier.background(Color(0xFF4CAF50))
+        // TODO: 2. scale the image to fill the screen with ContentScale.Crop
+        //            contentScale = ContentScale.Crop,
+        Image(
+            painter = painterResource(id = R.drawable.marseille),
+            contentDescription = null,
         )
-        Text(
-            text = "MARSEILLE",
-            modifier = Modifier.background(Color(0xFFFF9800))
-        )
-    }
-*/
 
+        Column(
+            modifier = Modifier
+        ) {
+            // TODO: 3. add styling to the text
+            //  change the text color to white: color = Color(0xFFFFFFFF),
+            //  increase the size: fontSize = 14.sp,
+            //  make the text more spacious: letterSpacing = 6.sp
+            Text(
+                text = "GREETINGS FROM",
+            )
+            // TODO: 4. add styling to the text:
+            //  color: white,
+            //  fontSize: 32.sp,
+            //  letterSpacing: 8.sp,
+            //  fontWeight: FontWeight.Bold
+            Text(
+                text = "MARSEILLE",
+            )
+
+        // TODO: 5. replace the default Modifier of the column to emphasize the text:
+        //       modifier = Modifier
+        //          .background(Color(0x40FFFFFF))
+        //          .padding(8.dp)
+
+        }
+    }
 }
 
 
