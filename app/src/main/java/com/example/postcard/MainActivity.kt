253281c5c86d43bd87f76afeb4a53d9f50a45908
nora <nborcsok@dev-id.fr>
2023-04-08 20:34:47 +0200

    phase 1: run project, change background color, change text

--- app/src/main/java/com/example/postcard/MainActivity.kt
+++ app/src/main/java/com/example/postcard/MainActivity.kt
@@ -4,24 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.postcard.ui.theme.PostcardTheme
+
+// TODO: 1. run the app (on an emulator or on a physical device)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PostcardTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    // TODO: 2. uncomment this line to change background color
+                    //color = Color(0xFFD3BDF8)
                 ) {
-                    Greeting("Android")
+                    // TODO: 3. write your name in the quotation marks
+                    Greeting(name = "Android")
                 }
             }
         }
@@ -30,5 +33,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    // TODO: 4. delete the text in the quotation marks and replace it with any text you want
     Text(text = "Hello $name!")
 }
