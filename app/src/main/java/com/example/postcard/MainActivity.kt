59deec1c2102219fb69ccd2ac6672e83db5fc612
nora <nborcsok@dev-id.fr>
2023-04-23 20:24:06 +0200

    phase 3: solution

--- app/src/main/java/com/example/postcard/MainActivity.kt
+++ app/src/main/java/com/example/postcard/MainActivity.kt
@@ -6,13 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,40 +38,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyComposable() {
     Box(Modifier.fillMaxSize()) {
-        // TODO: 1. add a Modifier to the image to fill the max height,
-        //            modifier = Modifier.fillMaxHeight(),
-
-        // TODO: 2. scale the image to fill the screen with ContentScale.Crop
-        //            contentScale = ContentScale.Crop,
         Image(
             painter = painterResource(id = R.drawable.marseille),
             contentDescription = null,
+            modifier = Modifier.fillMaxHeight(),
+            contentScale = ContentScale.Crop,
         )
 
         Column(
             modifier = Modifier
+                .background(Color(0x40FFFFFF))
+                .padding(8.dp)
         ) {
-            // TODO: 3. add styling to the text
-            //  change the text color to white: color = Color(0xFFFFFFFF),
-            //  increase the size: fontSize = 14.sp,
-            //  make the text more spacious: letterSpacing = 6.sp
             Text(
                 text = "GREETINGS FROM",
+                color = Color(0xFFFFFFFF),
+                fontSize = 14.sp,
+                letterSpacing = 6.sp
             )
-            // TODO: 4. add styling to the text:
-            //  color: white,
-            //  fontSize: 32.sp,
-            //  letterSpacing: 8.sp,
-            //  fontWeight: FontWeight.Bold
             Text(
                 text = "MARSEILLE",
+                color = Color(0xFFFFFFFF),
+                fontSize = 32.sp,
+                letterSpacing = 8.sp,
+                fontWeight = FontWeight.Bold
             )
-
-        // TODO: 5. replace the default Modifier of the column to emphasize the text:
-        //       modifier = Modifier
-        //          .background(Color(0x40FFFFFF))
-        //          .padding(8.dp)
-
         }
     }
 }
